5a5be82a3370c34cf62e87ebd03b9726c6a046fa
hiroyuki <11hi01ro30yuki@gmail.com>
2023-12-01 00:40:52 +0900

    データベースの接続を閉じる処理を追加

--- src/Review05.java
+++ src/Review05.java
@@ -11,62 +11,54 @@ public class Review05 {
 
     public static void main(String[] args) {
 
-        //必要な変数を宣言
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        String sql = null;
-
         try {
             // JDBCドライバをロード
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // mysqlに接続
-            con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
-                    "root",
-                    "rootroot");
-
-            // SQL文を変数にセット
-            sql = "SELECT * FROM person WHERE id = ? limit 1";
-
-            // キー入力を読み取って、変数にセット
+            // キー入力を読み取とって変数idに代入
             System.out.print("IDを入力してください > ");
-            int id= keyIn();
+            int id = keyIn();
 
-            //
-            pstmt = con.prepareStatement(sql);
+            // SQL文ひな形（?は変数idが代入される）
+            String sql = "SELECT * FROM person WHERE id = ? limit 1";
 
-            // SQL文の?にキーボード入力の値をセット
-            pstmt.setInt(1, id);
+            try (
+                    // DB接続
+                    Connection con = DriverManager.getConnection(
+                            "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true", 
+                            "root",
+                            "rootroot");
+                    PreparedStatement pstmt = con.prepareStatement(sql);
+                    ) {
 
-            // SQL実行
-            rs = pstmt.executeQuery();
+                // SQLの?にidを代入
+                pstmt.setInt(1, id);
 
-            // 実行結果からnameとageの値を取得
-            rs.next();
-            String name = rs.getString("Name");
-            int age = rs.getInt("age");
+                try (
+                        // SQLを実行
+                        ResultSet rs = pstmt.executeQuery();
+                        ) {
+                    // 実行結果を取得
+                    rs.next();
+                    String name = rs.getString("Name");
+                    int age = rs.getInt("age");
 
-            // 取得した値を表示
-            System.out.println(name);
-            System.out.println(age);
+                    // 取得した値を表示
+                    System.out.println(name);
+                    System.out.println(age);
+                }
 
-            //例外処理ブロック
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                rs.close();
             } catch (SQLException e) {
+                //
                 e.printStackTrace();
             }
+
+        } catch (ClassNotFoundException e) {
+            // JDBCドライバをロードに失敗した場合
+            e.printStackTrace();
         }
     }
 
-    // キーボード入力の値を取得（parseIntで数値に変換して取得）
     private static int keyIn() {
         String line = null;
         try {
