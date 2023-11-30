7d0c0eafa8e211c8da2d1200117e618b4adef1f0
hiroyuki <11hi01ro30yuki@gmail.com>
2023-11-30 20:43:06 +0900

    finallyブロックを追加

--- src/Review05.java
+++ src/Review05.java
@@ -10,55 +10,63 @@ import java.sql.SQLException;
 public class Review05 {
 
     public static void main(String[] args) {
+
+        //必要な変数を宣言
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         String sql = null;
-        
+
         try {
             // JDBCドライバをロード
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // DB接続
+            // mysqlに接続
             con = DriverManager.getConnection(
                     "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
                     "root",
                     "rootroot");
 
-            // キー入力を読み取って、idにセット
+            // SQL文を変数にセット
+            sql = "SELECT * FROM person WHERE id = ? limit 1";
+
+            // キー入力を読み取って、変数にセット
             System.out.print("IDを入力してください > ");
             int id= keyIn();
-            
-            // SQL文を準備（?の箇所はキーボード入力の値）
-            sql = "SELECT * FROM person WHERE id = ? limit 1";
-            
+
             //
             pstmt = con.prepareStatement(sql);
 
-
-
-            // SQL文を生成
+            // SQL文の?にキーボード入力の値をセット
             pstmt.setInt(1, id);
 
             // SQL実行
             rs = pstmt.executeQuery();
-            
-            // nameとageの値を取得
+
+            // 実行結果からnameとageの値を取得
             rs.next();
             String name = rs.getString("Name");
             int age = rs.getInt("age");
-            
+
             // 取得した値を表示
             System.out.println(name);
             System.out.println(age);
 
+            //例外処理ブロック
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    // キーボード入力の値を取得（parseIntで数値に変換して取得）
     private static int keyIn() {
         String line = null;
         try {
